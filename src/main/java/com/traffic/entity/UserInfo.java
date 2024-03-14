b9d2628b88ca317c055fbb16c9764bf4ec3932d1
wofov <hys950526@naver.com>
2024-03-14 13:47:35 +0900

    user생성 로직

--- src/main/java/com/traffic/entity/UserInfo.java
+++ src/main/java/com/traffic/entity/UserInfo.java
@@ -1,8 +1,14 @@
 package com.traffic.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class UserInfo {
 
     @Id
@@ -10,4 +16,17 @@ public class UserInfo {
     @Column(name = "user_info_id")
     private long id;
 
+    private String userName;
+    private String info;
+
+
+
+    public static UserInfo createUserInfo(String userName,
+                                          String info){
+
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserName(userName);
+        userInfo.setInfo(info);
+        return userInfo;
+    }
 }
