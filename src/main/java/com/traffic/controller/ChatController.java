e2841987d86f5e655eca99152d81c24f3cdbf65a
wofov <hys950526@naver.com>
2024-03-15 18:08:22 +0900

    웹소켓 controller

---
+++ src/main/java/com/traffic/controller/ChatController.java
@@ -0,0 +1,27 @@
+package com.traffic.controller;
+
+import com.traffic.dto.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+    @GetMapping("/chat-st")
+    public String chatStart(){
+        return "test";
+    }
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+}
